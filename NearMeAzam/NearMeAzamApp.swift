9f79d36edc54f5b042ac7da693f8090a1cd573d9
tanvirgeekios <tanvir.alam@iosventure.com>
2021-05-31 08:47:55 +0600

    Initial Commit

---
+++ NearMeAzam/NearMeAzamApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeAzamApp.swift
+//  NearMeAzam
+//
+//  Created by MD Tanvir Alam on 31/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NearMeAzamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
